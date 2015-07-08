2fe122c710a10d9692ebc5e8c2e09cf7704e3903
Patrick Schroeder <schroe9@gmail.com>
2015-07-08 03:05:31 -0700

    Cart controller and view to purchae books

--- Bookstore/public/js/main.js
+++ Bookstore/public/js/main.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
     window.location = '/manage/books';
   });
 
+  $('.removeCategory').click(function(e) {
+    deleteId = $(this).data('id');
+    $.ajax({
+      url: '/manage/categories/delete/' + deleteId,
+      type: 'DELETE',
+      success: function() {
+      }
+    });
+    window.location = '/manage/categories';
+  });
+
 });
