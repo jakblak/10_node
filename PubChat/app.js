9fd24de895537d879ec02cdec3ef346d0987a2f3
Patrick Schroeder <schroe9@gmail.com>
2015-07-09 13:26:34 -0700

    setup jQuery and socket.io to send messages

--- PubChat/app.js
+++ PubChat/app.js
@@ -5,7 +5,16 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+  // Send message
+  socket.on('send message', function(data) {
+    io.sockets.emit('new message', {
+      msg: data
+    });
+  });
 });
 
 server.listen(process.env.PORT || 3000);
