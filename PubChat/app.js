35a6475b3d6b95a32fe7afb5a1700ad739e8c8ca
Patrick Schroeder <schroe9@gmail.com>
2015-07-09 13:52:24 -0700

    finish chat app, users + errors

--- PubChat/app.js
+++ PubChat/app.js
@@ -8,12 +8,39 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+var usernames = [];
+
 io.sockets.on('connection', function(socket) {
+  //New User
+  socket.on('new user', function(data, cb) {
+    if(usernames.indexOf(data) != -1) {
+      cb(false);
+    } else {
+      cb(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+
+  function updateUsernames() {
+    io.sockets.emit('usernames', usernames);
+  }
+
   // Send message
   socket.on('send message', function(data) {
     io.sockets.emit('new message', {
-      msg: data
+      msg: data,
+      user: socket.username
     });
+  });
+
+  // Disconnect
+  socket.on('disconnect', function(data) {
+    if(!socket.username)
+      return;
+    usernames.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
   });
 });
 
